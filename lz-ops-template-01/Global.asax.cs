491d8f2a0ae433d45715468c9d5cd13f3cd12b18
patrickgrey <websemantic@yahoo.co.uk>
2023-02-10 15:52:58 +0000

    Cleaned up dependencies as some still referenced

--- lz-ops-template-01/Global.asax.cs
+++ lz-ops-template-01/Global.asax.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace lz_ops_template_01
@@ -15,7 +14,6 @@ namespace lz_ops_template_01
       AreaRegistration.RegisterAllAreas();
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
-      BundleConfig.RegisterBundles(BundleTable.Bundles);
     }
   }
 }
