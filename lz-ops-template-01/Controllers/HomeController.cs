8b6f0f06d73db7e8aab1907216726feb8c99bd0b
patrickgrey <websemantic@yahoo.co.uk>
2023-02-10 23:20:51 +0000

    Got server side API calls working

--- lz-ops-template-01/Controllers/HomeController.cs
+++ lz-ops-template-01/Controllers/HomeController.cs
@@ -1,16 +1,54 @@
-﻿using System;
+﻿using lz_ops_template_01.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace lz_ops_template_01.Controllers
 {
   public class HomeController : Controller
   {
-    public ActionResult Index()
+    //Hosted web API REST Service base url
+    string Baseurl = "https://www.thecocktaildb.com/api/";
+    public async Task<ActionResult> Index()
     {
-      return View();
+      Response CocktailData = new Response();
+      Drink firstDrink = new Drink();
+      using (var client = new HttpClient())
+      {
+        //Passing service base url
+        client.BaseAddress = new Uri(Baseurl);
+        client.DefaultRequestHeaders.Clear();
+        //Define request data format
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //Sending request to find web api REST service resource GetAllCocktailViewModels using HttpClient
+        HttpResponseMessage Res = await client.GetAsync("json/v1/1/lookup.php?i=11007");
+        //Checking the response is successful or not which is sent using HttpClient
+        if (Res.IsSuccessStatusCode)
+        {
+          //Storing the response details recieved from web api
+          var CocktailResponse = Res.Content.ReadAsStringAsync().Result;
+          //Deserializing the response recieved from web api and storing into the CocktailViewModel list
+          CocktailData = JsonConvert.DeserializeObject<Response>(CocktailResponse);
+          firstDrink = CocktailData.Drinks[0];
+          //System.Diagnostics.Debug.WriteLine("HIYAAAAAA");
+          //System.Diagnostics.Debug.WriteLine(firstDrink);
+          ViewBag.StrDrink = firstDrink.StrDrink;
+          ViewBag.StrGlass = firstDrink.StrGlass;
+          ViewBag.StrInstructions = firstDrink.StrInstructions;
+        }
+        else
+        {
+          ViewBag.StrDrink = "API call failed";
+          ViewBag.StrGlass = "API call failed";
+          ViewBag.StrInstructions = "API call failed";
+        }
+        //returning the employee list to view
+        return View();
+      }
     }
 
     public ActionResult About()
---
+++ lz-ops-template-01/Models/CocktailViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace lz_ops_template_01.Models
+{
+  public partial class Response
+  {
+    [JsonProperty("drinks")]
+    public IList<Drink> Drinks { get; set; }
+  }
+  public partial class Drink
+  {
+    [JsonProperty("idDrink")]
+    public string IdDrink { get; set; }
+    [JsonProperty("strDrink")]
+    public string StrDrink { get; set; }
+    [JsonProperty("strDrinkAlternate")]
+    public string StrDrinkAlternate { get; set; }
+    [JsonProperty("strTags")]
+    public string StrTags { get; set; }
+    [JsonProperty("strVideo")]
+    public string StrVideo { get; set; }
+    [JsonProperty("strCategory")]
+    public string StrCategory { get; set; }
+    [JsonProperty("strIBA")]
+    public string StrIBA { get; set; }
+    [JsonProperty("strAlcoholic")]
+    public string StrAlcoholic { get; set; }
+    [JsonProperty("strGlass")]
+    public string StrGlass { get; set; }
+    [JsonProperty("strInstructions")]
+    public string StrInstructions { get; set; }
+    [JsonProperty("strInstructionsES")]
+    public string StrInstructionsES { get; set; }
+    [JsonProperty("strInstructionsDE")]
+    public string StrInstructionsDE { get; set; }
+    [JsonProperty("strInstructionsFR")]
+    public string StrInstructionsFR { get; set; }
+    [JsonProperty("strInstructionsIT")]
+    public string StrInstructionsIT { get; set; }
+    [JsonProperty("strInstructionsZH-HANS")]
+    public string StrInstructionsZHHANS { get; set; }
+    [JsonProperty("strInstructionsZH-HANT")]
+    public string StrInstructionsZHHANT { get; set; }
+    [JsonProperty("strDrinkThumb")]
+    public string StrDrinkThumb { get; set; }
+    [JsonProperty("strIngredient1")]
+    public string StStrIngredient{ get; set; }
+    [JsonProperty("strIngredient2")]
+    public string StrIngredient2 { get; set; }
+    [JsonProperty("strIngredient3")]
+    public string StrIngredient3 { get; set; }
+    [JsonProperty("strIngredient4")]
+    public string StrIngredient4 { get; set; }
+    [JsonProperty("strIngredient5")]
+    public string StrIngredient5 { get; set; }
+    [JsonProperty("strIngredient6")]
+    public string StrIngredient6 { get; set; }
+    [JsonProperty("strIngredient7")]
+    public string StrIngredient7 { get; set; }
+    [JsonProperty("strIngredient8")]
+    public string StrIngredient8 { get; set; }
+    [JsonProperty("strIngredient9")]
+    public string StrIngredient9 { get; set; }
+    [JsonProperty("strIngredient10")]
+    public string StrIngredient10 { get; set; }
+    [JsonProperty("strIngredient11")]
+    public string StrIngredient11 { get; set; }
+    [JsonProperty("strIngredient12")]
+    public string StrIngredient12 { get; set; }
+    [JsonProperty("strIngredient13")]
+    public string StrIngredient13 { get; set; }
+    [JsonProperty("strIngredient14")]
+    public string StrIngredient14 { get; set; }
+    [JsonProperty("strIngredient15")]
+    public string StrIngredient15 { get; set; }
+    [JsonProperty("strIngredient")]
+    public string StrMeasure1 { get; set; }
+    [JsonProperty("strMeasure2")]
+    public string StrMeasure2 { get; set; }
+    [JsonProperty("strMeasure3")]
+    public string StrMeasure3 { get; set; }
+    [JsonProperty("strMeasure4")]
+    public string StrMeasure4 { get; set; }
+    [JsonProperty("strMeasure5")]
+    public string StrMeasure5 { get; set; }
+    [JsonProperty("strMeasure6")]
+    public string StrMeasure6{ get; set; }
+    [JsonProperty("strMeasure7")]
+    public string StrMeasure7 { get; set; }
+    [JsonProperty("strMeasure8")]
+    public string StrMeasure8 { get; set; }
+    [JsonProperty("strMeasure9")]
+    public string StrMeasure9 { get; set; }
+    [JsonProperty("strMeasure10")]
+    public string StrMeasure10 { get; set; }
+    [JsonProperty("strMeasure11")]
+    public string StrMeasure11 { get; set; }
+    [JsonProperty("strMeasure12")]
+    public string StrMeasure12 { get; set; }
+    [JsonProperty("strMeasure13")]
+    public string StrMeasure13 { get; set; }
+    [JsonProperty("strMeasure14")]
+    public string StrMeasure14 { get; set; }
+    [JsonProperty("strMeasure15")]
+    public string StrMeasure15 { get; set; }
+    [JsonProperty("strImageSource")]
+    public string StrImageSource { get; set; }
+    [JsonProperty("strImageAttribution")]
+    public string StrImageAttribution { get; set; }
+    [JsonProperty("strCreativeCommonsConfirmed")]
+    public string StrCreativeCommonsConfirmed { get; set; }
+    [JsonProperty("dateModified")]
+    public DateTime DateModified { get; set; }
+  }
+}
